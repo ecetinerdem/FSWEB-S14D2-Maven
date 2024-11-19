cfa4b5c463c1ac816dce243e35c9189e5eec104e
Cetin Erdem <cetoerdem1@gmail.com>
2024-11-19 15:16:02 +0400

    add Bed

--- src/main/java/org/example/model/Bed.java
+++ src/main/java/org/example/model/Bed.java
@@ -14,4 +14,40 @@ public class Bed {
         this.sheets = sheets;
         this.quilt = quilt;
     }
+
+    public void made() {
+        System.out.println("The bed is being made.");
+    }
+
+    public int getPillows() {
+        return this.pillows;
+    }
+
+    public void setPillows(int pillows) {
+        this.pillows = pillows;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getSheets() {
+        return this.sheets;
+    }
+
+    public void setSheets(int sheets) {
+        this.sheets = sheets;
+    }
+
+    public int getQuilt() {
+        return this.quilt;
+    }
+
+    public void setQuilt(int quilt) {
+        this.quilt = quilt;
+    }
 }
